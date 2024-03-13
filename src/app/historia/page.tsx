4e2858594cd2967cb08881cb9f0c2131d541cd31
Agustin Somoza Polchi <aspolchi@gmail.com>
2024-03-13 20:28:15 -0300

    Update page.tsx

--- src/app/historia/page.tsx
+++ src/app/historia/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
       <Founders />
       <Stadistics />
       <Testimonials />
-      <Interesed isFilter />
+      <Interesed  />
       <Footer />
     </main>
   );
