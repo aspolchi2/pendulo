73b8057440e82b0dc7321f0c71da9faae63b9161
Agustin Somoza Polchi <aspolchi@gmail.com>
2024-04-18 21:26:15 -0300

    a ver

--- src/app/historia/separator/HistoriaSep.tsx
+++ src/app/historia/separator/HistoriaSep.tsx
@@ -9,7 +9,7 @@ const hepta_slab = Hepta_Slab({
 const HistoriaSep = () => {
   return (
     <section
-      className={`sm:bg-historiaSep bg-historiaMobileDiv lg:bg-cover bg-cover lg:p-24 sm:p-12 lg:h-[60vh] h-max sm:justify-center lg:py-28 lg:px-28 text-white ${hepta_slab.className} flex flex-col sm:gap-8 h-dvh bg-center justify-end pb-12 text-center sm:text-left text-balance gap-2`}
+      className={`sm:bg-historiaSep bg-historiaMobileDiv lg:bg-cover bg-cover lg:p-24 sm:p-12 lg:h-[60vh] h-max sm:justify-center lg:py-28 lg:px-28 text-white ${hepta_slab.className} flex flex-col sm:gap-8 h-[600px] bg-center justify-end pb-12 text-center sm:text-left text-balance gap-2`}
     >
       <p className="lg:w-[790px] 2xl:mx-0 lg:text-2xl  md:text-xl sm:text-base text-xs  px-10">
         <span className="font-bold ">
